8b8f12cda8048a0d880a6fc39761732431e07c26
Yurii Tsudz <yura9989@gmail.com>
2022-12-20 12:09:36 +0200

    add template for gulp and add folder with elements'

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
 
+//Server
 gulp.task('browser', () => {
   browserSync.init({
     server: {
       port: 9000,
-      baseDir: "build"
+      baseDir: "source"
     }
   });
 
   gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+//Pug
+gulp.task('templates:compile', function buildHTML() {
+  return gulp.src('source/template/index.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('build'))
 })
